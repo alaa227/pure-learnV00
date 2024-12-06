36c774964220e24bf39503e65b0f0eeec8878834
alaa227 <alaakhalid227@gmail.com>
2024-12-06 08:17:39 +0200

    testt

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import { useState } from "react";
 
 import "./App.css";
 import Login from "./Auth/Login/Login";
-import SignIn from "./Auth/Sign in/SignIn";
+import SignUp from "./Auth/SignUp/SignUp";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Login />
-      <SignIn />
+      <SignUp/>
     </>
   );
 }
